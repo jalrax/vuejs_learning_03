1a27a236ff49a631b8b474dcd1e6b76323b29c34
jalrax <trymental@gmail.com>
2018-10-24 15:16:32 +0300

    added header and stocks elements

---
+++ src/components/stocks/Stocks.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <Stock v-for="stock in stocks" :stock="stock"></Stock>
+  </div>
+</template>
+
+<script>
+  import Stock from './Stock'
+
+  export default {
+    name: 'Stocks',
+    components: {
+      Stock,
+    },
+    data() {
+      return {
+        stocks: [
+          { id: 1, name: 'BMW', price: 110 },
+          { id: 2, name: 'Google', price: 200 },
+          { id: 3, name: 'Apple', price: 250 },
+          { id: 4, name: 'Twitter', price: 8 },
+        ],
+      }
+    },
+  }
+</script>
+
+<style scoped>
+
+</style>
--- src/views/About.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div class="about">
-    <h1>This is an about page</h1>
-    <p class="panel panel-default">Some text</p>
-  </div>
-</template>
